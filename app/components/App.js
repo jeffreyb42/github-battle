15a7344fd8c4e097418908ac3a140ecb30769ff9
jeffreyb42 <brassard.jeff@gmail.com>
2017-09-17 17:05:34 -0400

    Github-battle initial commit

---
+++ app/components/App.js
@@ -0,0 +1,31 @@
+const React = require('react');
+const ReactRouter = require('react-router-dom');
+const BrowserRouter = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
+const Nav = require('./Nav');
+const Home = require('./Home');
+const Battle = require('./Battle');
+const Popular = require('./Popular');
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={function () {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+module.exports = App;
